212107374f17ad2db59eaaecf179ac6f89076814
Filipa Flora <filipa.flora@gmail.com>
2023-08-03 17:34:48 +0100

    Lab Solved

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1/recipe-app";
 
 //Method 1 : Using Async Await
 
@@ -18,7 +18,46 @@ const manageRecipes = async () => {
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
 
-    // Run your code here, after you have insured that the connection was made
+    // create new recipe  
+    let recipe = await Recipe.create({
+      title: "Petit Gâteau",
+      level: 'Easy Peasy',
+      ingredients: ['200g SweetBitter Chocolate', '1/4 cup of sugar', '2 eggs', '2 spoons of unsalted butter', '2 spoons of flour', '2 egg yolks'],
+      dishType: 'dessert',
+      duration: 30,
+      creator: 'Suely Terezinha',
+      cuisine: "French",
+    });
+    
+
+    let petitGateau = await Recipe.find({title: 'Petit Gâteau'});
+    console.log(petitGateau)
+
+
+    // Insert the entire array of data
+    let allRecipesDb = await Recipe.insertMany(data);
+  
+
+    allRecipesDb.forEach((element) => {
+      console.log(element.title)
+    });
+    
+    // update 'Rigatoni alla Genovese'
+
+    let updatedRecipe = await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100})
+    let rigatoni = await Recipe.find({title: 'Rigatoni alla Genovese'})
+    console.log(rigatoni, 'Rigatoni alla Genovese successfully updated')
+
+    // Using the Model.deleteOne static, remove that recipe from the database and display a success message after doing it!
+
+    let removeRecipe = await Recipe.deleteOne({title: 'Carrot Cake'})
+    let carrotCake = await Recipe.find({title: 'Carrot Cake'})
+    console.log(carrotCake, 'Carrot Cake successfully deleted') 
+
+    // Close the database
+
+    const dbDisconnection = await mongoose.connection.close(MONGODB_URI);
+
   } catch (error) {
     console.log(error);
   }
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+  title: {
+    type: String,
+    required: true,
+  },
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+  level: {
+    type: String,
+    enum:['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+
+  ingredients: {
+    type: [String],
+  },
+
+  cuisine: {
+    type: String,
+    required: true,
+  },
+
+  dishType: {
+    type: String,
+    enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+  },
+
+  image: {
+    type: String,
+    default: "https://t1.uc.ltmcdn.com/pt/posts/7/1/9/como_fazer_petit_gateau_8917_orig.jpg",
+  },
+
+  duration: {
+    type: Number,
+    min: 0,
+  },
+
+  creator: {
+    type: String,
+  },
+
+  created: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
